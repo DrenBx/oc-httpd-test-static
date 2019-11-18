b6cc9cdf45f647536b6c401c6774d9189992f445
DrenBx <DrenBx@users.noreply.github.com>
2019-11-18 15:26:38 +0100

    Update categories.js

--- static/categories.js
+++ static/categories.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
   window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
     id: "featured",
     label: "Featured",
@@ -24,4 +24,22 @@
       ]
     }]
   });
+}());*/
+
+(function() {
+  var categories = [
+    {id: 'databases', label: 'Databases', subCategories: [
+      {id: 'mysql', label: 'MySQL', tags: ['mysql'], icon: 'icon-mysql-database'},
+      {id: 'postgresql', label: 'Postgres', tags: ['postgresql'], icon: 'icon-postgresql'},
+      {id: 'mariadb', label: 'MariaDB', tags: ['mariadb'], icon: 'icon-mariadb'}
+    ]},
+    {id: 'middleware', label: 'Middleware', subCategories: [
+      {id: 'runtimes', label: 'Runtimes & Frameworks', tags: ['eap', 'httpd', 'tomcat', 'nginx', 'jws']}
+    ]},
+    {id: 'cicd', label: 'CI/CD', subCategories: [
+      {id: 'jenkins', label: 'Jenkins', tags: ['jenkins'], icon: 'icon-jenkins'},
+      {id: 'pipelines', label: 'Pipelines', tags: ['pipelines'], icon: 'fa fa-clone'}
+    ]
+  ];
+  _.set(window, 'OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES', categories);
 }());
