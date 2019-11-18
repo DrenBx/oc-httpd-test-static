2f3890e8dddf5eb9c75784985b977887bed83318
DrenBx <DrenBx@users.noreply.github.com>
2019-11-18 14:04:59 +0100

    Update categories.js

--- static/categories.js
+++ static/categories.js
@@ -1,18 +1,12 @@
-// Add a Featured category as the first category tab.
-window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
-  // Required. Must be unique.
-  id: "featured",
-  // Required
-  label: "Featured",
-  subCategories: [
+(function() {
+  window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
+    id: "featured",
+    label: "Featured",
+    subCategories: [
     {
-      // Required. Must be unique.
       id: "go",
-      // Required.
       label: "Go",
-      // Optional. If specified, defines a unique icon for this item.
       icon: "icon-go-gopher",
-      // Required. Items matching any tag will appear in this subcategory.
       tags: [
         "go",
         "golang",
@@ -20,18 +14,14 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
       ]
     },
     {
-      // Required. Must be unique.
       id: "jenkins",
-      // Required.
       label: "Jenkins",
-      // Optional. If specified, defines a unique icon for this item.
       icon: "icon-jenkins",
-      // Required. Items matching any tag will appear in this subcategory.
       tags: [
         "jenkins",
         "nginx",
         "httpd"
       ]
-    }
-  ]
-});
+    }]
+  });
+}());
